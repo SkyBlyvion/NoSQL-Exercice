ae7f0cee68f59033634e27abda2cdb52d0f462fa
SkyBlyvion <richtofen60@gmail.com>
2024-09-20 16:53:55 +0200

    add routes

--- server/routes/route.js
+++ server/routes/route.js
@@ -16,4 +16,44 @@ router.get("/", async (req, res) => {
     }
 });
 
+// route pour un seul publication
+router.get("/publication/:id*", async (req, res) => {
+    let collection = db.collection('publis');
+    // let publication = await collection.findOne({ "_id": req.params.id }); // aller chercher le query string identifié par id 
+    if (!publication) {
+        res.status(404).send("La publication n''existe pas");
+    } else {
+        res.status(200).send(publication);
+    }
+});
+
+// route pour ajouter une publication
+// TODO: corriger
+router.post("/addPub", async (req, res) => {
+   try {
+    let collection = db.collection('publis');
+    let publication = req.body;
+    let result = await collection.insertOne(publication);
+    if(result){
+        res.status(200).send('la publication est ajoutée');
+    } else {
+        res.status(500).send("une erreur est survenue lors de l''insertion");
+    }
+   } catch (error) {
+    res.status(500).send(`une erreur est survenue : ${error}`);
+   } 
+});
+
+// mise a jour de la publication
+router.put("/editPub/:id", async (res, req) => {
+    try {
+        let collection = db.collection("publis");
+        let publication = req.body;
+        let result = await collection.updateOne({"_id": req.params.id}, {$set: publication});
+        res.statut(200).send("la publicxation est moidifie avec succés")
+    } catch (error) {
+        res.status(500).send(`une erreur est survenue : ${error}`);
+    } 
+});
+
 export default router;
